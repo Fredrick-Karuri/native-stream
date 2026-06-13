0c33134b59993e9b3c2814d98c5c313b6a7f63d8
Fredrick Karuri <fredrickkmaish@gmail.com>
2026-06-13 18:34:52 +0300

    chore(android): bump version to 1.0.8

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nativestream.android"
         minSdk = 26          // API 26+
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.7"
+        versionCode = 10
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
