5243c1420f3bcf4e99af394612bf6df41d83cb72
Fredrick Karuri <fredrickkmaish@gmail.com>
2026-06-28 15:30:14 +0300

    chore(android): bump version to 1.1.2

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nativestream.android"
         minSdk = 26          // API 26+
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.1.1"
+        versionCode = 15
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
